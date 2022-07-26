42ada0d806ecf4987dbab36f123c2cca2f33ced5
Ryannx <50788804+Ryannx@users.noreply.github.com>
2022-07-25 23:07:53 -0700

    07/25/2022 240 (#279)
    * dfs
    
    * dfs
    
    * rm
    
    * bst
    
    * ll
    
    * interval
    
    * interval
    
    * rm
    
    * interval
    
    * rm
    
    * graph
    
    * rm
    
    * dp
    
    * tp
    
    * rm
    
    * dfs
    
    * rm
    
    * stack
    
    * dfs
    
    * rm
    
    * dfs
    
    * dp
    
    * tree
    
    * tree
    
    * rm
    
    * bit
    
    * ll
    
    * 231
    
    * stack
    
    * qs
    
    * rm
    
    * tree
    
    * heap
    
    * rm
    
    * qs
    
    * tree
    
    * tree
    
    * rm
    
    * other
    
    * rm
    
    * treeNdp
    
    * rm
    
    * dpNstack
    
    * rm
    
    * o
    
    * lots
    
    * remove 1463
    
    * rm
    
    * design
    
    * rm
    
    * google
    
    * rm
    
    * combol
    
    * combol
    
    * rm
    
    * graph
    
    * rm
    
    * Dijkstra
    
    * dijkstra
    
    * dijkstra
    
    * dijkstra
    
    * rm
    
    * greedy
    
    * bs
    
    * tree
    
    * rm
    
    * bs
    
    * dijkstra
    
    * other
    
    * linkedlist
    
    * other
    
    * design
    
    * bs
    
    * rollingHash
    
    * tree
    
    * tree&rollingHash
    
    * tree
    
    * rm
    
    * rollinghash
    
    * divide&conquer
    
    * fix
    
    * design
    
    * design
    
    * divideNconquer & mst
    
    * graph
    
    * uf
    
    * uf
    
    * rolling hash
    
    * segtree
    
    * tp
    
    * fix
    
    * 1526
    
    * remove out307
    
    * 1649
    
    * remove segtree out
    
    * 1649 rm
    
    * 1649 rm upadte
    
    * 1649 rm fix
    
    * fix SegTreeNode class
    
    * remove segtree out
    
    * 1157
    
    * remove out
    
    * 1209
    
    * 456
    
    * 370 SegTree and diff array
    
    * 218
    
    * add 699
    
    * remove out 218
    
    * add 1641
    
    * add 47
    
    * add 1489
    
    * add 1579
    
    * add 2275
    
    * add 2276
    
    * update 499
    
    * add 1091
    
    * update rm
    
    * add 787
    
    * add 2093
    
    * add 2135 & update TrieNode class
    
    * add 2015
    
    * add 647
    
    * add 2280
    
    * add 2279
    
    * add 474
    
    * add 2281
    
    * add dp32 and fix stack32
    
    * add 2237
    
    * add 354
    
    * add interval 218
    
    * update rm
    
    * add interval 2158
    
    * add 2290
    
    * add 2289
    
    * add 29
    
    * add 351 1461
    
    * add 1893
    
    * add 1405
    
    * add 984
    
    * add 621
    
    * add 1386
    
    * update 269 dfs
    
    * update rm
    
    * add 2296
    
    * add 160
    
    * add 1048
    
    * add 947
    
    * add 2286
    
    * remove out 2296
    
    * add 1057 & flipBinaryString segTree
    
    * add 1987
    
    * add 664
    
    * add 1658
    
    * add 818 && 2305
    
    * update Dijkstra  2305 to 2304
    
    * add 2305
    
    * add 1695
    
    * add 1151
    
    * add binary search 2305
    
    * update rm
    
    * add 1197
    
    * add 2178
    
    * add binarySearch 4
    
    * add dp 583
    
    * add 2306
    
    * add 527
    
    * update dfs 464
    
    * add trie 527
    
    * add 968
    
    * add trie 745
    
    * delete production 527
    
    * add trie 1268 & other 2310
    
    * delete product 527 & 745
    
    * update rm
    
    * add trie 820
    
    * update trie 820
    
    * add 2312
    
    * add 1642
    
    * add greedy 630
    
    * add other 1354
    
    * rename 1354 rm
    
    * add 665
    
    * add 1423
    
    * add dp 2320
    
    * add dp 2321
    
    * add 1647
    
    * update rm
    
    * add greedy  406
    
    * add 1851
    
    * add greedy 462
    
    * add dp 256
    
    * add greedy 1465
    
    * add greedy 376
    
    * add greedy 135
    
    * add greedy 128
    
    * add greedy 128
    
    * add dp 10
    
    * add dp 1473
    
    * add deque 239 & 1696
    
    * add 473 576 629 1696 239 2342
    
    * fix
    
    * update rm and deque.iml
    
    * add tree 236
    
    * add  binary search 240
    
    Co-authored-by: Haifeng Xu <haifengxu@Haifengs-MacBook-Pro.local>
    Co-authored-by: Haifeng Xu <haifengxu@macbook-pro.lan>
    Co-authored-by: Haifeng Xu <haifengxu@ryan-mbp.lan>

---
+++ BinarySearch/src/LC_240/LC_240.java
@@ -0,0 +1,25 @@
+package LC_240;
+
+public class LC_240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        if (matrix == null) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = m - 1;
+        int j = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
